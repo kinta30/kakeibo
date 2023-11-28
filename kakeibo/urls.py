91e3f1b92880b56caed6cc337a22302ece805781
kinta30 <kiyomokinta@gmail.com>
2023-11-28 18:07:04 +0900

    編集削除月間支出 11/28

--- kakeibo/urls.py
+++ kakeibo/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('income_list/', views.IncomeList.as_view(), name='income_list'),
     path('payment_create/',views.PaymentCreate.as_view(), name='payment_create'),
     path('income_create/',views.IncomeCreate.as_view(), name='income_create'),
-    path('payment_delete/',views.PaymentDelete.as_view(), name='payment_delete'),
-    path('Monthly_Payment/',views.MonthlyPayment.as_view(), name='Monthly_Payment'),
+    path('payment_delete/<int:pk>',views.PaymentDelete.as_view(), name='payment_delete'),
+    path('income_delete/<int:pk>',views.IncomeDelete.as_view(), name='income_delete'),
+    path('payment_update/<int:pk>',views.PaymentUpdate.as_view(), name='payment_update'),
+    path('income_update/<int:pk>',views.IncomeUpdate.as_view(), name='income_update'),
+    path('month/<int:year>/<int:month>/',views.MonthlyPayment.as_view(), name='Monthly_Payment'),
     path('transition/',views.transition.as_view(), name='transition')
 ]
--- kakeibo/views.py
+++ kakeibo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView,CreateView,TemplateView,DeleteView,UpdateView
 
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy,reverse
 from .models import Payment,PaymentCategory,Income,IncomeCategory
 # from .forms import IncomeCreate
  
@@ -29,20 +29,40 @@ class IncomeCreate(CreateView):
     model = Income
     context_object_name ="Income"
     fields = '__all__'
-    success_url = reverse_lazy("payment_list")
+    success_url = reverse_lazy("income_list")
 
 class PaymentDelete(DeleteView):
     model = Payment
     context_object_name ="payment"
+    success_url = reverse_lazy("payment_list")
 
-class PaymentUpddate(UpdateView):
+class IncomeDelete(DeleteView):
+    model = Payment
+    context_object_name ="Income"
+    success_url = reverse_lazy("income_list")
+
+class PaymentUpdate(UpdateView):
     model = Payment
     context_object_name ="payment"
+    fields = '__all__'
+    success_url = reverse_lazy("payment_list")
 
+class IncomeUpdate(UpdateView):
+    model = Payment
+    context_object_name ="Income"
+    fields = '__all__'
+    success_url = reverse_lazy("income_list")
 
 class MonthlyPayment(TemplateView):
     model = Payment
     template_name = "Monthly_Payment.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['year'] = 2023
+        context['month'] = 11
+        context['monthly_payment_url'] = reverse('Monthly_Payment', kwargs={'year': context['year'], 'month': context['month']})
+        return context
 
 class transition(TemplateView):
     model = Payment
